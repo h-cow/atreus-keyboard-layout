ee6f3fb377429ab9dfc06e8ff8221497e658fe45
MouseZero <rm@mousezero.com>
2018-11-30 01:25:21 -0800

    Added better support for the window manager

--- keyboards/atreus/keymaps/mousezero/keymap.c
+++ keyboards/atreus/keymaps/mousezero/keymap.c
@@ -13,13 +13,14 @@
 #define _MD 3
 #define _CH 4
 #define _DK 5
+#define _WINDOW_MANAGER 6
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  LT(_DK, KC_SLSH) ,
-    MT(MOD_HYPR, KC_ESC), KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,      KC_LALT, KC_LCTL,        LT(_MD, KC_SPC),  MO(_RS), KC_MINS, KC_QUOT, MT(MOD_LGUI, KC_ENT)
+    LT(_WINDOW_MANAGER, KC_ESC), KC_TAB, KC_LGUI,  KC_LSFT, MT(MOD_LGUI, KC_BSPC),      KC_LALT, KC_LCTL,        LT(_MD, KC_SPC),  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
   ),
 
   /*
@@ -53,17 +54,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_CH] = LAYOUT( /* Qwerty */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    LGUI(LSFT(KC_N)),    KC_O,    KC_P    ,
-    KC_A,    KC_S,    LGUI(LALT(KC_I)),    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
+    KC_Q,    LGUI(KC_W),    KC_E,    KC_R,    KC_T,                            KC_Y,    LGUI(KC_L),    LGUI(LSFT(KC_N)),    KC_O,    KC_P    ,
+    KC_A,    KC_S,    LGUI(LALT(KC_I)),    KC_F,    KC_G,                      LCTL(LSFT(KC_TAB)),    KC_TAB,    LSFT(KC_TAB),    LCTL(KC_TAB),    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    LGUI(LALT(KC_B)),                      KC_N,    KC_M,    KC_COMM, LGUI(KC_MINS),  LGUI(KC_PLUS) ,
     TO(_QW), KC_NO, KC_NO,  KC_NO, KC_NO,         KC_NO, KC_NO,              KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO
   ),
 
   [_DK] = LAYOUT( /* [> RAISE <] */
     KC_EXLM, LGUI(LSFT(KC_7)),    LGUI(LSFT(KC_8)),   LGUI(LSFT(KC_9)), KC_RCBR,                   KC_PGUP, LGUI(KC_7),    LGUI(KC_8),   LGUI(KC_9), KC_ASTR ,
-    KC_HASH, LGUI(LSFT(KC_4)),    LGUI(LSFT(KC_5)),   LGUI(LSFT(KC_6)), KC_DLR,                    KC_PGDN, LGUI(KC_4),    LGUI(KC_5),   LGUI(KC_6), KC_PLUS ,
+    KC_HASH, LGUI(LSFT(KC_4)),    LGUI(LSFT(KC_5)),   LGUI(LSFT(KC_6)), LGUI(LSFT(KC_0)),                    LGUI(KC_0), LGUI(KC_4),    LGUI(KC_5),   LGUI(KC_6), KC_PLUS ,
     KC_LBRC, LGUI(LSFT(KC_1)),    LGUI(LSFT(KC_2)),   LGUI(LSFT(KC_3)), KC_AMPR,                   KC_GRV,  LGUI(KC_1),    LGUI(KC_2),   LGUI(KC_3), KC_BSLS ,
-    TO(_LW), KC_NO,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  )
+    TO(_LW), KC_NO,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
+
+  [_WINDOW_MANAGER] = LAYOUT( /* Qwerty */
+    HYPR(KC_Q),    LGUI(KC_W),    HYPR(KC_E),    HYPR(KC_R),    HYPR(KC_T),                      HYPR(KC_Y),    HYPR(KC_U),    HYPR(KC_I),    HYPR(KC_O),    HYPR(KC_P    ),
+    HYPR(KC_A),    HYPR(KC_S),    HYPR(KC_D),    HYPR(KC_F),    HYPR(KC_G),                      HYPR(KC_H),    HYPR(KC_J),    HYPR(KC_K),    HYPR(KC_L),    HYPR(KC_SCLN ),
+    HYPR(KC_Z),    HYPR(KC_X),    HYPR(KC_C),    HYPR(KC_V),    HYPR(KC_B),                      HYPR(KC_N),    HYPR(KC_M),    HYPR(KC_COMM), HYPR(KC_DOT),  HYPR(KC_SLSH),
+    KC_TRNS,       KC_NO,        KC_NO,          KC_NO,         KC_NO,       KC_TAB, KC_LGUI,       KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO
+  )
+
 };
 
 const uint16_t PROGMEM fn_actions[] = {

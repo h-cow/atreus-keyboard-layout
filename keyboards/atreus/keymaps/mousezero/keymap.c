f507d0a2e5b55ae7287dc76253de5886ff91a9c8
MouseZero <rm@mousezero.com>
2018-12-12 05:54:34 -0800

    The 2 middle buttons are only defined by the first layer

--- keyboards/atreus/keymaps/mousezero/keymap.c
+++ keyboards/atreus/keymaps/mousezero/keymap.c
@@ -31,7 +31,8 @@ enum custom_keycodes {
     TMUX_CLOSE_WINDOW,
     LINUX_AS_DEFAULT,
     MAC_AS_DEFAULT,
-    TMUX_RENAME_TAB
+    TMUX_RENAME_TAB,
+    TMUX_SCROLL
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -73,6 +74,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       case TMUX_RENAME_TAB:
         SEND_STRING(SS_LCTRL("b")","SS_LCTRL("u"));
         return false;
+      case TMUX_SCROLL:
+        SEND_STRING(SS_LCTRL("b")"[");
+        return false;
       case LINUX_AS_DEFAULT:
         set_single_persistent_default_layer(_MAIN_LINUX);
         return false;
@@ -96,20 +100,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  LT(_VSCODE, KC_SLSH) ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, MT(MOD_LCTL, KC_BSPC),      KC_LALT, KC_LCTL,    KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, LT(_TMUX, KC_ENT)
+    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, MT(MOD_LCTL, KC_BSPC),      KC_TRNS, KC_TRNS,    KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, LT(_TMUX, KC_ENT)
   ),
 
   [_RS] = LAYOUT( /* [> RAISE <] */
     KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
     KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
     KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
-    TG(_LW), TG(_SHORTCUT_SWITCHER),  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
+    TG(_LW), TG(_SHORTCUT_SWITCHER),  KC_LGUI, KC_LSFT, KC_BSPC, KC_TRNS, KC_TRNS, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
 
   [_LW] = LAYOUT( /* [> LOWER <] */
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
     KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
     KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    TO(_MAIN_MAC),   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_MAIN_MAC), KC_PSCR, KC_SLCK, KC_PAUS ),
+    TO(_MAIN_MAC),   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_TRNS, KC_TRNS, KC_SPC,  TO(_MAIN_MAC), KC_PSCR, KC_SLCK, KC_PAUS ),
 
   [_VSCODE] = LAYOUT(
     LGUI(LALT(LSFT(KC_1))), LGUI(LSFT(KC_E)), LGUI(LSFT(KC_E)), LGUI(LALT(KC_F)), LGUI(LCTL(LSFT(KC_R))), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LGUI(KC_P),
@@ -134,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_TMUX] = LAYOUT(
     KC_Q,    TMUX_CLOSE_WINDOW,    KC_E,    TMUX_RENAME_TAB,    KC_T,                            TMUX_NEW_TAB,    TMUX_SPLIT_HORIZONTAL,    TMUX_SPLIT_VERTICAL,    TMUX_NEW_TAB,    TMUX_NEXT_WINDOW    ,
-    KC_A,    KC_S,    LGUI(LSFT(KC_TAB)),    LGUI(KC_TAB),    KC_G,                      TMUX_MOVE_LEFT,    TMUX_MOVE_DOWN,    TMUX_MOVE_UP,    TMUX_MOVE_RIGHT,    TMUX_PREVIOUS_WINDOW ,
+    KC_A,    TMUX_SCROLL,    LGUI(LSFT(KC_TAB)),    LGUI(KC_TAB),    KC_G,                      TMUX_MOVE_LEFT,    TMUX_MOVE_DOWN,    TMUX_MOVE_UP,    TMUX_MOVE_RIGHT,    TMUX_PREVIOUS_WINDOW ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_NO,                      KC_N,    KC_M,    KC_COMM, KC_NO,  KC_NO ,
     KC_TRNS, KC_NO, KC_NO,  KC_NO, KC_NO,         KC_NO, KC_NO,              KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO
   ),

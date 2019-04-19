2e0224f749ebb4fd032f7da6b86575550e6e0d12
MouseZero <rm@mousezero.com>
2019-04-18 23:32:28 -0700

    even a better mouse

--- keyboards/atreus/keymaps/mousezero/config.h
+++ keyboards/atreus/keymaps/mousezero/config.h
@@ -3,12 +3,13 @@
 // place overrides here
 #define TAPPING_TERM 250
 
-#define MOUSEKEY_DELAY             150
-#define MOUSEKEY_INTERVAL          75
-#define MOUSEKEY_MAX_SPEED         65
-#define MOUSEKEY_TIME_TO_MAX       50
+#define MOUSEKEY_DELAY             0
+#define MOUSEKEY_INTERVAL          20
+#define MOUSEKEY_MAX_SPEED         7
+#define MOUSEKEY_TIME_TO_MAX       60
 #define MOUSEKEY_WHEEL_MAX_SPEED   50
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+
 
 #define MK_3_SPEED
 #define MK_MOMENTARY_ACCEL // comment this out for tap-to-select acceleration

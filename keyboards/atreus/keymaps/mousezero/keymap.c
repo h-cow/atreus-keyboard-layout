14dbea7c7271ab95063d7575560fcd226a8622b2
MouseZero <rm@mousezero.com>
2019-05-11 15:13:33 -0700

    swap the tab and alt

--- keyboards/atreus/keymaps/mousezero/keymap.c
+++ keyboards/atreus/keymaps/mousezero/keymap.c
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    LT(_MOUSE, KC_ESC), KC_LALT, KC_TAB,  KC_LSFT, MT(MOD_LGUI, KC_BSPC),   KC_LGUI, KC_LCTL,       KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
+    LT(_MOUSE, KC_ESC), KC_TAB, KC_LALT,  KC_LSFT, MT(MOD_LCTL, KC_BSPC),   KC_LGUI, KC_LCTL,       KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
   ),
 
   [_MAIN_LINUX] = LAYOUT( /* Qwerty */
